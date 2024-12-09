d04138a8b346d3285052412419aa593405a58460
BARRE-COQUEL David <dbarrecoquel@datasolution.fr>
2024-12-09 15:38:44 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,4 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+export default defineConfig({ plugins: [react(), ssr()] });
